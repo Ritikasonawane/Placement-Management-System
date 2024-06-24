622d53579108c2ff65bf2371552d536444493860
Ritikasonawane <94622850+Ritikasonawane@users.noreply.github.com>
2024-06-24 16:22:18 +0530

    Add files via upload

---
+++ Drives/about.php
@@ -0,0 +1,245 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<!--favicon-->
+        <link rel="shortcut icon" href="favicon.ico" type="image/icon">
+        <link rel="icon" href="favicon.ico" type="image/icon">
+<title>Drives :: About</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Tillage Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- bootstarp-css -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!--// bootstarp-css -->
+<!-- css -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--// css -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!--fonts-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,800,700,600' rel='stylesheet' type='text/css'>
+<!--/fonts-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+	 new WOW().init();
+</script>
+</head>
+<body>
+	<!-- banner -->
+	<div class="banner a-banner">
+		<!-- container -->
+		<div class="container">
+			<div class="header">
+				<div class="head-logo">
+					
+				</div>
+				<div class="top-nav">
+					<span class="menu"><img src="images/menu.png" alt=""></span>
+					<ul class="nav1">
+						<li class="hvr-sweep-to-bottom">
+								<a href="../Homepage/home.php">MET BKC <i>Goto Placement Homepage</i></a>
+							</li>
+						<li class="hvr-sweep-to-bottom">
+								<a href="index.php">Home<i>Get the Overview of the Site</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom active">
+								<a href="about.php">About<i>About Us and our Cheif Architects</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom">
+								<a href="products.php">Campus Drives<i>Campus Drives</i></a>
+							</li>
+							
+							<li class="hvr-sweep-to-bottom">
+								<a href="mail.php">Mail Us<i>Get in Touch with us Instantly</i></a>
+							</li>
+							<div class="clearfix"></div>
+						<div class="clearfix"> </div>
+					</ul>
+					<!-- script-for-menu -->
+							 <script>
+							   $( "span.menu" ).click(function() {
+								 $( "ul.nav1" ).slideToggle( 300, function() {
+								 // Animation complete.
+								  });
+								 });
+							</script>
+						<!-- /script-for-menu -->
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //banner -->
+	<!-- about -->
+	<div class="about-top">
+		<!-- container -->
+		<div class="container">
+			<div class="about-info wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+				<h3>A few words about us</h3>
+				<h5>This is what tells about who we are and about Pioneers of the Institution </span></h5>
+			</div>
+			<div class="banner-bottom-grids a-banner-bottom-grids">
+				<div class="col-md-5 banner-bottom-right wow fadeInRight animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					<img src="images/met.jpg" alt="">
+				</div>
+				<div class="col-md-7 a-banner-bottom-text">
+					<div class="jumbotron banner-bottom-left wow fadeInLeft animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					  <h3>Placement and Training Department</h3>
+						<h5></span></h5>
+						<p>The MET Group of Educational Institutes, spread over Maharashtra have a presence to reckon with. The Group is benchmarked in every aspect. The entire MET Group has convincingly evolved, into the most sought after education destination. Every Institute is duly accredited by NBA, approved by AICTE and affiliated to Savitribai Phule Pune University. Its MET. Every Institute has an Independent presence with enviable state-of-the-art infrastructure and all the facilities and resources, to label it an ideal place for learning. It Comprises Institutes in its fold, run by capable and qualified resource people, drawn from various disciplines. The entire MET Educational conglomerate continues in its search for excellence.
+						</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //about -->
+	<!-- about-bottom -->
+	<div class="about-bottom">
+		<!-- container -->
+		<div class="container">
+			<h3 class="wow fadeInRight animated" style="visibility: visible;">MET
+Vision & Mission</h3>
+			<div class="about-bottom-grids">
+				<div class="col-md-6 about-bottom-left wow fadeInLeft animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					<h4>MET Vision</h4>
+					<p>To develop Institute as a Centre of Excellence for developing globally accepted professionals having application orientation, lifelong learning approach towards socio-economic growth of stake holders and the Society as a whole.</p>
+				</div>
+				<div class="col-md-6 about-bottom-left about-bottom-right wow fadeInRight animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					<h4>MET Mission</h4>
+					<p>Preparing Graduates capable enough to practice effectively and responsibly to technological changes and pursue advanced engineering.
+Inculcate applied research to support and contribute to the social and economic well-being of citizens and organizations in nearby region.
+Cultivate the entrepreneurial spirit and linkage of engineering education and business for technology commercialization.
+Evolve thoughtfully to the changing needs of industry and surrounding world.</p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //about-bottom -->
+	<!-- team -->
+	<div class="team">
+		<!-- container -->
+		<div class="container">
+			<div class="team-info">
+				<h3 class="wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">Meet Our Team</h3>
+				<div class="team-grids">
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/Bhujbal.jpg" alt="">
+						<h6>Shri.Chhagan Chandrakant Bhujbal</h6>
+						<p> Founder President, BKC Institutes</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/Safali.jpg" alt="">
+						<h6>Dr. (Mrs.) Sefali Bhujbal</h6>
+						<p>Founder Secretary, BKC Institutes</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/pri.jpeg" alt="">
+						<h6>Dr. V.P.Wani</h6>
+						<p> Principle Of BKC Institutes</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/hop.jpeg" alt="">
+						<h6>Dr. P.D.Jadhav</h6>
+						<p>HOD OF BKC MCA Institutes</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //team -->
+	<!-- footer -->
+		<div class="footer">
+			<!-- container -->
+			<div class="container">
+				<div class="col-md-6 footer-left  wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<ul>
+						<li>
+							<a href="index.php">Home</a>
+						</li>
+						<li>
+							<a href="about.php">About</a>
+						</li>
+						<li>
+							<a href="products.php">Campus Drives</a>
+						</li>
+						<li>
+							<a href="../Gallery/index.html">Gallery</a>
+						</li>
+						<li>
+							<a href="mail.php">Mail Us</a>
+						</li>
+					</ul>
+					<form>
+						<input type="text" placeholder="Email" required="">
+						<input type="submit" value="Subscribe">
+					</form>
+				</div>
+				<div class="col-md-3 footer-middle wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<h3>Address</h3>
+					<div class="address">
+						<p>Aadgaon
+							<span>Nashik</span>
+						</p>
+					</div>
+					<div class="phone">
+						<p>+91 9158263125</p>
+					</div>
+				</div>
+				<div class="col-md-3 footer-right  wow fadeInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<h6 style="color: white">MET Institute of Engineering</h6>
+					<h4>
+						<a href="../Homepage/home.php" style="color: white">Placement Management System</a>
+					</h4>
+					<p></p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<!-- //container -->
+		</div>
+		<!-- //footer -->
+		<div class="copyright">
+			<!-- container -->
+			<div class="container">
+				<div class="copyright-left wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					 <p>Copyright &copy; 2022-23 MET  | Developed by
+              <a href="http://znumerique.azurewebsites.net" target="_parent">SSPD Technologies</a>
+					</p>
+				</div>
+				<div class="copyright-right wow fadeInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<ul>
+						<li>
+							<a href="#" class="twitter"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter facebook"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter chrome"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter pinterest"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter linkedin"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter dribbble"> </a>
+						</li>
+					</ul>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<!-- //container -->
+		</div>
+</body>
+</html>
